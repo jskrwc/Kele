b1cc93e96e9c193204a27fcc8fcbbf53070ad3d1
Jim Knopf <jsk4@mac.com>
2018-06-23 16:12:29 -0700

    Checkpoint 2 - intitialization completed

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,6 +1,23 @@
+require 'httparty'
+
+
 class Kele
   include HTTParty
 
-  base_uri 'https://www.bloc.io/api/v1'
+  def initialize(email, password)
+
+    response = self.class.post(base_uri("sessions"), body: {
+      email: email,
+      password: password
+    })
+    @auth_token = response['auth_token']
+    raise "Invalid email or password - try again!" if @auth_token.nil?
+  end
+
+  private
+
+  def base_uri(endpoint)
+    "https://www.bloc.io/api/v1/#{endpoint}"
+  end
 
 end
