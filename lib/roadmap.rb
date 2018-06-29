9752b8f55e13c11877e82607a942d2dcf3f04b0b
Jim Knopf <jsk4@mac.com>
2018-06-29 11:47:18 -0400

    Remaining Checkpoints completed

--- lib/roadmap.rb
+++ lib/roadmap.rb
@@ -12,4 +12,12 @@ module Roadmap
     JSON.parse(response.body)
   end
 
-end  
+  def get_remaining_checkpoints(chain_id)
+    response = self.class.get(base_uri("enrollment_chains/#{chain_id}/checkpoints_remaining_in_section"),
+    headers: { "authorization" => @auth_token })
+    # puts response.success?
+    # puts response.inspect
+    JSON.parse(response.body)
+  end
+
+end
